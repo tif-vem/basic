7d3c60e1378c14ab87a5d1110601f242df424292
Yevhenii Starchenko <yevhenii.starchenko@ivkhk.ee>
2025-09-17 20:03:32 +0300

    encapsulation

--- oop.js
+++ oop.js
@@ -32,3 +32,36 @@ genericAnimal.displayInfo();
 myDog.displayInfo();
 myDog.speak();
 
+//2. Инкапсуляция (Encapsulation)
+//Инкапсуляция - скрытие внутренней реализации от внешнего мира
+//или же - обьединение данных и методов
+//и работы с этими данными в одном классе
+console.log('--- Инкапсуляция ---');
+
+class BankAccount {
+    //# - означает что свойство приватное
+    // private property
+    #balance =0; //приватное свойство
+    constructor(initialBalance) {
+        if(initialBalance > 0) {
+            this.#balance = initialBalance;
+        }
+    }
+    //публичный метод для внесения денег
+    deposit(amount) {
+        if(amount > 0) {
+            this.#balance += amount;
+            console.log(`внесено: ${amount}.новый баланс: ${this.#balance}`);
+        }
+    }
+    //публичный метод для снятия
+    withdraw(amount) {
+        if(amount > 0 && amount <= this.#balance) {
+            this.#balance -= amount;
+            console.log(`снято ${amount}. новый баланс: ${this.#balance}`);
+        } else {
+            console.log(' недостаточно средств или неверная суммаю');
+        }
+    }
+    
+}
